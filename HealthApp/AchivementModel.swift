72aba06d9a81802d72d15cf05ff0bfb8537b89cd
Milton <milleung964@gmail.com>
2016-05-01 13:06:08 -0700

    Added constance and empty achivement model

---
+++ HealthApp/AchivementModel.swift
@@ -0,0 +1,18 @@
+//
+//  AchivementModel.swift
+//  HealthApp
+//
+//  Created by Milton Leung on 2016-05-01.
+//  Copyright © 2016 Milton Leung. All rights reserved.
+//
+
+import Foundation
+
+protocol AchievementProtocol {
+    
+}
+
+extension ModelInterface: AchievementProtocol {
+    
+
+}
---
+++ HealthApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  HealthApp
+//
+//  Created by Milton Leung on 2016-05-01.
+//  Copyright © 2016 Milton Leung. All rights reserved.
+//
+
+import Foundation
+
+var achievements = [1: "Baby Steps", 3: "You have reached 3 km!", 5: "You have reached 5 km!"]
