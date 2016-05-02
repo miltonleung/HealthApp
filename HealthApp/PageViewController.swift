819cb2f8142c1029f05b71de6c70155de9fecdef
Milton <milleung964@gmail.com>
2016-05-01 22:13:59 -0700

    added container view

---
+++ HealthApp/PageViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PageViewController.swift
+//  HealthApp
+//
+//  Created by Milton Leung on 2016-05-01.
+//  Copyright © 2016 Milton Leung. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    var pages = [UIViewController]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        let stepPage: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("stepPage")
+        let distancePage: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("distancePage")
+        
+        pages.append(stepPage)
+        pages.append(distancePage)
+        
+        setViewControllers([stepPage], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.indexOf(viewController)!
+        let previousIndex = abs((currentIndex - 1) % pages.count)
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.indexOf(viewController)!
+        let nextIndex = abs((currentIndex + 1) % pages.count)
+        return pages[nextIndex]
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+}
