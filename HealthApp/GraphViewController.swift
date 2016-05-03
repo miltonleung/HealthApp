d35c40a3a620229ef74026983daf209bd447730c
Milton <milleung964@gmail.com>
2016-05-03 16:14:52 -0700

    implemented transparent background

--- HealthApp/GraphViewController.swift
+++ HealthApp/GraphViewController.swift
@@ -20,6 +20,9 @@ class GraphViewController: UIViewController {
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         
+        
+        self.view.backgroundColor = UIColor.clearColor()
+        barChartView.backgroundColor = UIColor.clearColor()
         setChart(months, values: unitsSold)
         
     }
@@ -37,5 +40,16 @@ class GraphViewController: UIViewController {
         let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
         let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
         barChartView.data = chartData
+        
+        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 0)
+        barChartView.descriptionText = ""
+        barChartView.animate(yAxisDuration: 3.0)
+        barChartView.gridBackgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 0)
+
+        barChartView.drawGridBackgroundEnabled = true
+        
+        chartDataSet.colors = [UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 0.88)]
+        chartDataSet
+
     }
 }
